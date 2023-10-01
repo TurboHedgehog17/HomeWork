cdad30ed6b1f41c9d01683160a5689cdfdd24a3d
Koroleva <JuMe@DESKTOP-37R47B0>
2023-10-01 14:45:25 +0300

    Добавьте файлы проекта.

---
+++ DZ/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.WriteLine("Королёва 3-09ПС-1");
+            Console.ReadLine();
+        }
+    }
+}
