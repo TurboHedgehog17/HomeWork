09610b70056a8f03a07b70b752fdc987c4872cb6
Koroleva <juliakoroleva2k17@mail.ru>
2023-10-01 14:51:56 +0300

    commit

--- DZ/Program.cs
+++ DZ/Program.cs
@@ -12,7 +12,7 @@ namespace DZ
         {
             Console.WriteLine("Hello world");
             Console.WriteLine("Королёва 3-09ПС-1");
-            Console.WriteLine("Какие-то изменения");
+            Console.WriteLine("Какие-то изменения ааааааа");
             Console.ReadLine();
         }
     }
